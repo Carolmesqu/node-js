1c6b10e3c35eb8eaa5f7b91224d967dbb7fc6334
carolmesqu <carolmesqu@gmail.com>
2023-06-14 19:31:59 -0300

    DEV: Node JS criptografia e tokens - Aula Alura

---
+++ node-js-criptografia-e-tokens-JWT/hash/hash.js
@@ -0,0 +1,34 @@
+import { createHash } from 'crypto';
+
+function criaHash(senha) {
+    return createHash('sha256').update(senha).digest('hex');
+}
+
+console.log(criaHash('uma String Qualquer'));
+
+class Usuario {
+    constructor(nome, senha) {
+        this.nome = nome;
+        this.hash = criaHash(senha);
+    }
+
+    autentica(nome, senha) {
+        if(nome === this.nome && this.hash === criaHash(senha)) {
+            console.log("Usuário autenticado com sucesso!");
+            return true;
+        }
+        console.log("Usuário ou senha incorretos.");
+        return false;
+    }
+}
+
+const usuario = new Usuario('Maria Carolina','123456');
+
+console.log(usuario);
+
+// Caso de sucesso
+usuario.autentica('Maria Carolina','123456');
+
+// Casos de fracasso
+usuario.autentica('Maria','123456');
+
